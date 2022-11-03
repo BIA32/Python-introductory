3ee38bd823bdf9e458521e1ff47ae64c5017f554
Ben <baziz10sa@gmail.com>
2022-11-03 13:11:42 -0700

    python

--- withdrawmoney.py
+++ withdrawmoney.py
@@ -1,14 +1,14 @@
-print ("How much would you like to withdraw?")
+current_balance = 100
+amount = int(input ("how  much do you take? "))
 
 def withdraw_money (current_balance, amount):
     if (current_balance >= amount):
         current_balance = current_balance - amount
-    print ("insufficient funds")
-current_balance = 100
+        print ("sufficient funds")
+        print ("current balance now: ", current_balance)
+    else:
+        print ("insufficient funds")
 
-withdraw_money = input ("")
+withdraw_money(current_balance,amount)
 
-if withdraw_money <= current_balance:
-    print ("Here is your money")
-else:
-    print ("insufficient funds")
+
